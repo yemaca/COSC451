5435b691a05ee264bce55d4a41983d37bd0d15c1
yemaca <155181233+yemaca@users.noreply.github.com>
2024-11-20 16:12:29 -0500

    possible end of maze solution
    links for report
    wiring schematic

--- LineFollowingRobot.ino
+++ LineFollowingRobot.ino
@@ -1,19 +1,23 @@
-//black high white low
+// black high white low
 #define LW1 2
-#define LW2 3     //wheel positions
-#define RW1 4     //LW2 and RW1 high to turn forward
+#define LW2 3  // wheel positions
+#define RW1 4  // LW2 and RW1 high to turn forward
 #define RW2 5
 
-#define enaLW 11  //enable motor control for wheels
+#define enaLW 11  // enable motor control for wheels
 #define enaRW 12
 
 #define RIGHT 6
-#define FRONT 7   //sensor positions
+#define FRONT 7  // sensor positions
 #define LEFT 8
 
 int R = 0;
-int F = 0;        //variables to store sensor readings
+int F = 0;  // variables to store sensor readings
 int L = 0;
+
+char path[128];
+char optimized[32];
+int index = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -26,6 +30,39 @@ void setup() {
   pinMode(RW2, OUTPUT);
   pinMode(enaLW, OUTPUT);
   pinMode(enaRW, OUTPUT);
+}
+
+void updatePath(char c) {
+  path[index] = c;
+  index++;
+}
+
+// shorten robot path using pre-defined rules
+void minimizePath() {
+  int currPath = 0;  // current index of path array
+  int currOpt = 0;   // current index of optimized array
+
+  while (path[currPath] != 'E') {
+    if (path[currPath] == 'L' && path[currPath + 1] == 'B' && path[currPath + 2] == 'R') {         // LBR --> B
+      optimized[currOpt] = 'B';
+    } else if (path[currPath] == 'L' && path[currPath + 1] == 'B' && path[currPath + 2] == 'S') {  // LBS --> R
+      optimized[currOpt] = 'R';
+    } else if (path[currPath] == 'R' && path[currPath + 1] == 'B' && path[currPath + 2] == 'L') {  // RBL --> B
+      optimized[currOpt] = 'B';
+    } else if (path[currPath] == 'F' && path[currPath + 1] == 'B' && path[currPath + 2] == 'L') {  // FBL --> R
+      optimized[currOpt] = 'R';
+    } else if (path[currPath] == 'L' && path[currPath + 1] == 'B' && path[currPath + 2] == 'R') {  // FBF --> B
+      optimized[currOpt] = 'B';
+    } else if (path[currPath] == 'L' && path[currPath + 1] == 'B' && path[currPath + 2] == 'R') {  // LBL --> F
+      optimized[currOpt] = 'F';
+    } else {
+      optimized[currOpt] = path[currPath];
+    }
+
+    currPath++;
+    currOpt++;
+  }
+  optimized[currOpt] = 'E';
 }
 
 // turn right until line is found
@@ -41,10 +78,10 @@ void findLine() {
   }
 }
 
-//move left wheels forward and right wheels backwards
+// move left wheels forward and right wheels backwards
 void turnRight() {
   Serial.write("turn right\n");
-  
+
   digitalWrite(LW2, HIGH);
   digitalWrite(RW2, HIGH);
 
@@ -52,9 +89,11 @@ void turnRight() {
 
   digitalWrite(RW2, LOW);
   digitalWrite(LW2, LOW);
+
+  updatePath('R');
 }
 
-// //move right wheels forward and left wheels backwards
+// move right wheels forward and left wheels backwards
 void turnLeft() {
   Serial.write("turn left\n");
 
@@ -65,25 +104,29 @@ void turnLeft() {
 
   digitalWrite(RW1, LOW);
   digitalWrite(LW1, LOW);
+
+  updatePath('L');
 }
 
-// //turn left twice to spin
+// turn left twice to spin
 void spin() {
   Serial.write("spin\n");
-  
+
   digitalWrite(RW1, HIGH);
   digitalWrite(LW1, HIGH);
-  
+
   delay(860);
 
   digitalWrite(RW1, LOW);
   digitalWrite(LW1, LOW);
+
+  updatePath('B');
 }
 
-// //move all wheels forward
+// move all wheels forward
 void moveForward() {
   Serial.write("move forward\n");
-  
+
   digitalWrite(LW2, HIGH);
   digitalWrite(RW1, HIGH);
 
@@ -91,8 +134,11 @@ void moveForward() {
 
   digitalWrite(LW2, LOW);
   digitalWrite(RW1, LOW);
+
+  updatePath('F');
 }
 
+// stop all movement
 void halt() {
   Serial.write("stopping\n");
 
@@ -102,37 +148,82 @@ void halt() {
   digitalWrite(RW1, LOW);
 }
 
+// move slightly forward to check if end of maze
+void checkEnd() {
+  Serial.write("check end\n");
+
+  digitalWrite(LW2, HIGH);
+  digitalWrite(RW1, HIGH);
+
+  delay(300);
+
+  digitalWrite(LW2, LOW);
+  digitalWrite(RW1, LOW);
+}
+
 void loop() {
-  //read from sensors
+  // enable motor control
   digitalWrite(enaLW, HIGH);
   digitalWrite(enaRW, HIGH);
-  
+
+  // read from sensors
   L = digitalRead(LEFT);
   R = digitalRead(RIGHT);
   F = digitalRead(FRONT);
 
-  if (L == LOW && R == LOW && F == LOW) {             //spin if at dead end (000)
+  if (L == LOW && R == LOW && F == LOW) {           // spin if at dead end (000)
     spin();
     moveForward();
-  } else if (L == LOW && R == HIGH && F == LOW) {     //turn right if only option & move forward (001)
+  } else if (L == LOW && R == HIGH && F == LOW) {   // turn right if only option & move forward (001)
     turnRight();
     moveForward();
-  } else if (L == LOW && R == LOW && F == HIGH) {     //move forward if only option (010)
+  } else if (L == LOW && R == LOW && F == HIGH) {   // move forward if only option (010)
     moveForward();
-  } else if (L == LOW && R == HIGH && F == HIGH) {    //move forward if can go F/R (011)
+  } else if (L == LOW && R == HIGH && F == HIGH) {  // move forward if can go F/R (011)
     moveForward();
-  } else if (L == HIGH && R == LOW && F == LOW) {     //turn left if only option & move forward (100)
+  } else if (L == HIGH && R == LOW && F == LOW) {   // turn left if only option & move forward (100)
     turnLeft();
     moveForward();
-  } else if (L == HIGH && R == HIGH && F == LOW) {    //turn left if can go L/R (101)
+  } else if (L == HIGH && R == HIGH && F == LOW) {  // turn left if can go L/R (101)
     turnLeft();
     moveForward();
-  } else if (L == HIGH && R == LOW && F == HIGH) {    //turn left if can go L/F (110)
+  } else if (L == HIGH && R == LOW && F == HIGH) {  // turn left if can go L/F (110)
     turnLeft();
     moveForward();
-  } else {                                            //either turn left or at end of maze (111)
+  } else {                                          // either turn left or at end of maze (111)
     Serial.write("done/turn left\n");
+
+    checkEnd();                                     // checkEnd to move slightly forward and check if sensors still all black
+    L = digitalRead(LEFT);  
+    R = digitalRead(RIGHT);                         // if L/R sensor are low then there was a turn that was missed by moving forward
+    F = digitalRead(FRONT);
+
+    if (L == HIGH && R == HIGH && F == HIGH) {      // end of maze
+      updatePath('E');
+      minimizePath();
+    } else {                                        // turn left at + intersection
+      turnLeft();
+      moveForward();
+    }
   }
 
+  // ensure robot is back on line
+  if ((F = digitalRead(FRONT)) == LOW)
+    findLine();
+
   delay(3000);
+
+  // moveForward();
+  // delay(2000);
+  // turnLeft();
+  // delay(2000);
+  // turnRight();
+  // delay(2000);
+  // spin();
+  // delay(5000);
 }
+
+/*
+  for end of maze idea - https://www.hackster.io/Varun2905/maze-solving-robot-3-ir-sensors-9ada3b
+  path minimization - https://patrickmccabemakes.com/tutorials/Maze_Solving/
+*/
