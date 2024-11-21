7fd09061e473f3917ca659548b7219d9c1643d29
yemaca <155181233+yemaca@users.noreply.github.com>
2024-11-21 16:35:58 -0500

    Add files via upload

--- LineFollowingRobot.ino
+++ LineFollowingRobot.ino
@@ -43,7 +43,7 @@ void minimizePath() {
   int currOpt = 0;   // current index of optimized array
 
   while (path[currPath] != 'E') {
-    if (path[currPath] == 'L' && path[currPath + 1] == 'B' && path[currPath + 2] == 'R') {         // LBR --> B
+    if (path[currPath] == 'L' && path[currPath + 1] == 'B' && path[currPath + 2] == 'R') {  // LBR --> B
       optimized[currOpt] = 'B';
     } else if (path[currPath] == 'L' && path[currPath + 1] == 'B' && path[currPath + 2] == 'S') {  // LBS --> R
       optimized[currOpt] = 'R';
@@ -65,28 +65,21 @@ void minimizePath() {
   optimized[currOpt] = 'E';
 }
 
-// turn right until line is found
-void findLine() {
-  while ((F = digitalRead(FRONT)) == LOW) {
-    digitalWrite(LW2, HIGH);
-    digitalWrite(RW2, HIGH);
-
-    delay(100);
-
-    digitalWrite(RW2, LOW);
-    digitalWrite(LW2, LOW);
-  }
-}
-
 // move left wheels forward and right wheels backwards
 void turnRight() {
   Serial.write("turn right\n");
 
+  // move forward slightly to turn closer to line
+  digitalWrite(LW2, HIGH);
+  digitalWrite(RW1, HIGH);
+  delay(150);
+  digitalWrite(LW2, LOW);
+  digitalWrite(RW1, LOW);
+
+  // turn right
   digitalWrite(LW2, HIGH);
   digitalWrite(RW2, HIGH);
-
-  delay(430);
-
+  delay(125);
   digitalWrite(RW2, LOW);
   digitalWrite(LW2, LOW);
 
@@ -97,13 +90,27 @@ void turnRight() {
 void turnLeft() {
   Serial.write("turn left\n");
 
+  // move forward slightly to turn closer to line
+  digitalWrite(LW2, HIGH);
   digitalWrite(RW1, HIGH);
-  digitalWrite(LW1, HIGH);
-
-  delay(430);
-
+  delay(150);
+  digitalWrite(LW2, LOW);
   digitalWrite(RW1, LOW);
-  digitalWrite(LW1, LOW);
+
+  // turn left
+  while ((F = digitalRead(FRONT)) == LOW) {
+    digitalWrite(LW1, HIGH);
+    digitalWrite(RW1, HIGH);
+
+    delay(25);
+
+    digitalWrite(RW1, LOW);
+    digitalWrite(LW1, LOW);
+
+    delay(200);
+  }
+
+  findLine('L');
 
   updatePath('L');
 }
@@ -123,20 +130,98 @@ void spin() {
   updatePath('B');
 }
 
+// turn right until line is found
+void findLine(char c) {
+  delay(500);
+  // char b = path[index];
+
+  // if (c == 'L' || b == 'L') {
+  //   while ((F = digitalRead(FRONT)) == LOW) {
+  //     digitalWrite(LW1, HIGH);
+  //     digitalWrite(RW1, HIGH);
+
+  //     delay(25);
+
+  //     digitalWrite(RW1, LOW);
+  //     digitalWrite(LW1, LOW);
+
+  //     delay(200);
+  //   }
+  // } else {
+  //   while ((F = digitalRead(FRONT)) == LOW) {
+  //     digitalWrite(LW2, HIGH);
+  //     digitalWrite(RW2, HIGH);
+
+  //     delay(25);
+
+  //     digitalWrite(RW2, LOW);
+  //     digitalWrite(LW2, LOW);
+
+  //     delay(200);
+  //   }
+  // }
+
+  if ((L = digitalRead(LEFT)) == HIGH) {
+    turnLeft();
+    return;
+  }
+
+  if (path[index-1] == 'L') {
+    digitalWrite(LW1, HIGH);
+    digitalWrite(RW1, HIGH);
+
+    delay(25);
+
+    digitalWrite(RW1, LOW);
+    digitalWrite(LW1, LOW);
+  }
+
+  while ((F = digitalRead(FRONT)) == LOW) {
+    digitalWrite(LW2, HIGH);
+    digitalWrite(RW2, HIGH);
+
+    delay(25);
+
+    digitalWrite(RW2, LOW);
+    digitalWrite(LW2, LOW);
+
+    delay(200);
+  }
+}
+
 // move all wheels forward
 void moveForward() {
-  Serial.write("move forward\n");
-
-  digitalWrite(LW2, HIGH);
-  digitalWrite(RW1, HIGH);
-
-  delay(3000);
-
-  digitalWrite(LW2, LOW);
-  digitalWrite(RW1, LOW);
-
   updatePath('F');
+
+  while ((F = digitalRead(FRONT)) == HIGH) {
+    digitalWrite(LW2, HIGH);
+    digitalWrite(RW1, HIGH);
+    delay(50);
+
+    digitalWrite(LW2, LOW);
+    digitalWrite(RW1, LOW);
+
+    findLine('F');
+
+    if ((L = digitalRead(LEFT)) == HIGH) {
+      break;
+    }
+  }
 }
+
+// void moveForward() {
+//   Serial.write("move forward\n");
+
+//   digitalWrite(LW2, HIGH);
+//   digitalWrite(RW1, HIGH);
+
+//   delay(200);
+
+//   digitalWrite(LW2, LOW);
+//   digitalWrite(RW1, LOW);
+
+//   updatePath('F');
+// }
 
 // stop all movement
 void halt() {
@@ -162,6 +247,7 @@ void checkEnd() {
 }
 
 void loop() {
+  delay(5000);
   // enable motor control
   digitalWrite(enaLW, HIGH);
   digitalWrite(enaRW, HIGH);
@@ -171,47 +257,62 @@ void loop() {
   R = digitalRead(RIGHT);
   F = digitalRead(FRONT);
 
-  if (L == LOW && R == LOW && F == LOW) {           // spin if at dead end (000)
-    spin();
-    moveForward();
-  } else if (L == LOW && R == HIGH && F == LOW) {   // turn right if only option & move forward (001)
-    turnRight();
-    moveForward();
-  } else if (L == LOW && R == LOW && F == HIGH) {   // move forward if only option (010)
-    moveForward();
-  } else if (L == LOW && R == HIGH && F == HIGH) {  // move forward if can go F/R (011)
-    moveForward();
-  } else if (L == HIGH && R == LOW && F == LOW) {   // turn left if only option & move forward (100)
+  if (L == HIGH)
     turnLeft();
+  else
     moveForward();
-  } else if (L == HIGH && R == HIGH && F == LOW) {  // turn left if can go L/R (101)
-    turnLeft();
-    moveForward();
-  } else if (L == HIGH && R == LOW && F == HIGH) {  // turn left if can go L/F (110)
-    turnLeft();
-    moveForward();
-  } else {                                          // either turn left or at end of maze (111)
-    Serial.write("done/turn left\n");
 
-    checkEnd();                                     // checkEnd to move slightly forward and check if sensors still all black
-    L = digitalRead(LEFT);  
-    R = digitalRead(RIGHT);                         // if L/R sensor are low then there was a turn that was missed by moving forward
-    F = digitalRead(FRONT);
+  // if (L == HIGH) {
+  //   turnLeft();
+  //   delay(500);
+  //   F = digitalRead(FRONT);
+  //   if (F == LOW)
+  //     findLine();
+  // }
+  // else if (F == HIGH)
+  //   moveForward();
 
-    if (L == HIGH && R == HIGH && F == HIGH) {      // end of maze
-      updatePath('E');
-      minimizePath();
-    } else {                                        // turn left at + intersection
-      turnLeft();
-      moveForward();
-    }
-  }
+  // if (L == LOW && R == LOW && F == LOW) {           // spin if at dead end (000)
+  //   spin();
+  //   moveForward();
+  // } else if (L == LOW && R == HIGH && F == LOW) {   // turn right if only option & move forward (001)
+  //   turnRight();
+  //   moveForward();
+  // } else if (L == LOW && R == LOW && F == HIGH) {   // move forward if only option (010)
+  //   moveForward();
+  // } else if (L == LOW && R == HIGH && F == HIGH) {  // move forward if can go F/R (011)
+  //   moveForward();
+  // } else if (L == HIGH && R == LOW && F == LOW) {   // turn left if only option & move forward (100)
+  //   turnLeft();
+  //   moveForward();
+  // } else if (L == HIGH && R == HIGH && F == LOW) {  // turn left if can go L/R (101)
+  //   turnLeft();
+  //   moveForward();
+  // } else if (L == HIGH && R == LOW && F == HIGH) {  // turn left if can go L/F (110)
+  //   turnLeft();
+  //   moveForward();
+  // } else {                                          // either turn left or at end of maze (111)
+  //   Serial.write("done/turn left\n");
 
-  // ensure robot is back on line
-  if ((F = digitalRead(FRONT)) == LOW)
-    findLine();
+  //   checkEnd();                                     // checkEnd to move slightly forward and check if sensors still all black
+  //   L = digitalRead(LEFT);
+  //   R = digitalRead(RIGHT);                         // if L/R sensor are low then there was a turn that was missed by moving forward
+  //   F = digitalRead(FRONT);
 
-  delay(3000);
+  //   if (L == HIGH && R == HIGH && F == HIGH) {      // end of maze
+  //     updatePath('E');
+  //     minimizePath();
+  //   } else {                                        // turn left at + intersection
+  //     turnLeft();
+  //     moveForward();
+  //   }
+  // }
+
+  // // ensure robot is back on line
+  // if ((F = digitalRead(FRONT)) == LOW)
+  //   findLine();
+
+  // delay(3000);
 
   // moveForward();
   // delay(2000);
